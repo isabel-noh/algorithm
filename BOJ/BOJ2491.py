e7c68876992f9400dc192d3a56c0b79dc7e3003e
isabel-noh <nohhyunjeong93@gmail.com>
2022-08-09 18:22:14 +0900

    boj2491

--- BOJ/BOJ2491.py
+++ BOJ/BOJ2491.py
@@ -1,25 +1,32 @@
-# 0에서부터 9까지의 숫자로 이루어진 N개의 숫자가 나열된 수열이 있다. 
-# 그 수열 안에서 연속해서 커지거나(같은 것 포함), 
-# 혹은 연속해서 작아지는(같은 것 포함) 수열 중 가장 길이가 긴 것을 찾아내어 
-# 그 길이를 출력하는 프로그램을 작성하라. 
-
-# 예를 들어 수열 1, 2, 2, 4, 4, 5, 7, 7, 2 의 경우에는 
-# 1 ≤ 2 ≤ 2 ≤ 4 ≤ 4 ≤ 5 ≤ 7 ≤ 7 이 가장 긴 구간이 되므로 그 길이 8을 출력한다. 
-# 수열 4, 1, 3, 3, 2, 2, 9, 2, 3 의 경우에는 3 ≥ 3 ≥ 2 ≥ 2 가 가장 긴 구간이 되므로 
-# 그 길이 4를 출력한다. 
-# 또 1, 5, 3, 6, 4, 7, 1, 3, 2, 9, 5 의 경우에는 연속해서 
-# 커지거나 작아지는 수열의 길이가 3 이상인 경우가 없으므로 2를 출력하여야 한다.
-
-# 입력
-# 첫째 줄에는 수열의 길이 N이 주어지고, 둘째 줄에는 N개의 숫자가 빈칸을 사이에 두고 주어진다. 
-# N은 1 이상 100,000 이하의 정수이다.
-
-# 출력
-# 첫째 줄에 가장 긴 길이를 출력한다.
-
-# --- 
-
-N = int(input()) #수열의 길이
+N = int(input()) #수열의 길이 9
 num_list = input().split()  # 1, 2, 2, 4, 4, 5, 7, 7, 2 
-max_length = 0
-length = 0
+
+# 뒤집은 숫자 배열
+num_flipped_list = num_list[::-1]
+# 최종 수열 중 가장 긴 길이
+max_len = 0
+max_len_list = []
+
+for i in range(N): # 0 ~ 8
+    length = 1
+    for j in range(i, N-1):  # 0, 1, 2, 3, 4, 5, 6, 7
+        if num_list[j] <= num_list[j+1]: # 다음 숫자가 지금 숫자보다 크거나 같으면 
+            length += 1 # length 에 1 추가
+        else : # 아니면 for문 끝내! 
+            break
+    max_len_list.append(length)
+    # if length >= max_len:
+    #     max_len = length   
+
+# for i in range(N):
+#     length = 1
+#     for j in range(i, N-1):
+#         if num_flipped_list[j] <= num_flipped_list[j+1]:
+#             length += 1
+#         else : # 아니면 for문 끝내! 
+#             break
+#     # if length >= max_len:
+#     #     max_len = length   
+#     max_len_list.append(length)
+
+# print(max(max_len_list))
