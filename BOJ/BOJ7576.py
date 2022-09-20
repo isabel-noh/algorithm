f8fdae407ea84eef54350a3618899a6ce31dd881
isabel-noh <nohhyunjeong93@gmail.com>
2022-09-20 16:10:47 +0900

    0919

--- BOJ/BOJ7576.py
+++ BOJ/BOJ7576.py
@@ -1,8 +1,4 @@
-
-import sys
 from collections import deque
-
-sys.setrecursionlimit(1000000) #1000000번 재귀가 가능하도록 변경하기
 
 # 보관 후 `하루`가 지나면, 익은 토마토들의 인접한 곳에 있는 익지 않은 토마토들은 익은 토마토의 영향을 받아 익게 된다. 하나의 토마토의 인접한 곳은 왼쪽, 오른쪽, 앞, 뒤 네 방향에 있는 토마토를 의미한다. 
 di = [1, 0, -1, 0]
@@ -28,17 +24,7 @@ def ripen(a, b):
                 # print(bat)
                 count += 1
                 ripen(a+di[d], b+dj[d])
-# def ripen(tomato):
-#     while tomato:
-#         i, j = tomato.pop(0)
-#         for d in range(4):
-#             ni, nj = i+di[d], j+dj[d]
-#             if 0 <= ni < N and 0 <= nj < M:
-#                 if bat[ni][nj] == 0:
-#                     tomato.append((ni, nj))
-#                     bat[ni][nj] = bat[i][j] + 1
-#                 elif bat[ni][nj] == -1:
-#                     pass
+
 for i in range(len(queue)):
     ripen(queue[i][0], queue[i][1])
 
@@ -56,49 +42,3 @@ if count == -1 :
     print(count)
 else:
     print(max(max_)-1)
-
-# ------------------------------
-# import sys
-# from collections import deque
-
-# sys.setrecursionlimit(1000000) #1000000번 재귀가 가능하도록 변경하기
-
-# queue = deque()
-
-# di = [1, 0, -1, 0]
-# dj = [0, 1, 0, -1] 
-# M, N = map(int, input().split())
-# bat = [list(map(int, input().split())) for _ in range(N)]
-
-# count = 0
-# for i in range(N):
-#     for j in range(M):
-#         if bat[i][j] == 1:
-#             queue.append((j,i))
-
-# def ripen(queue):
-#     while queue:
-#         i, j = queue.popleft()
-#         for d in range(4):
-#             ni, nj = i+di[d], j+dj[d]
-#             if 0 <= ni < N and 0 <= nj < M:
-#                 if bat[ni][nj] == 0:
-#                     queue.append((ni, nj))
-#                     bat[ni][nj] = bat[i][j] + 1
-#                 elif bat[ni][nj] == -1:
-#                     pass
-# ripen(queue)
-
-# max_ = []
-# for i in bat:
-#     a = max(i)
-#     max_.append(a)
-# # 모두 익어있으면 0 모두 익지 못하면 -1  
-# for i in bat:
-#     if 0 in i:
-#         count = -1
-#         break
-# if count == -1 :
-#     print(count)
-# else:
-#     print(max(max_)-1)
