1332a7e6c212af8e54edc25e0a2a6cf6eafa9a02
isabel-noh <nohhyunjeong93@gmail.com>
2023-04-18 09:22:00 +0900

    2668 onprogress

--- BOJ/ddd.py
+++ BOJ/ddd.py
@@ -1,6 +1,7 @@
-while True:
-    try:
-        str = input()
-        print(str)
-    except:
-        break
+str = list(input())
+for l in range(len(str)):
+    if ord(str[l]) > 97: 
+        str[l] = (chr(ord(str[l])-32))
+    else: 
+        str[l] = (chr(ord(str[l])+32))
+print(*str, sep='', end='')
