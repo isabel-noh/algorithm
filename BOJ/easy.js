365ca863f16b68cb2d64085064dcf90b838e714b
isabel-noh <nohhyunjeong93@gmail.com>
2023-07-10 15:55:29 +0900

    nQueen backtracking

--- BOJ/easy.js
+++ BOJ/easy.js
@@ -1,47 +1,46 @@
-// import sys
-// sys.stdin = open('./sample.txt')
-
-// def solution(num, a):
-//     dp = [[0] * (num) for _ in range(2)]
-//     for i in range(num):
-//         if i == 0:
-//             dp[0][i], dp[1][i] = a[0][i], a[1][i]
-//         elif i == 1:
-//             dp[0][i] = dp[1][i-1] + arr[0][i]
-//             dp[1][i] = dp[0][i-1] + arr[1][i]
-//         else:
-//             dp[0][i] = arr[0][i] + max(dp[1][i-1], dp[0][i-2], dp[1][i-2])
-//             dp[1][i] = arr[1][i] + max(dp[0][i-1], dp[0][i-2], dp[1][i-2])
-//     result = max(max(dp[0]), max(dp[1]))
-//     return result
-
-// T = int(input())
-// for t in range(T):
-//     n = int(input())
-//     arr = [list(map(int, input().split())) for _ in range(2)]
-//     print(solution(n, arr))
-
 const fs = require("fs");
-const [a, b] = fs.readFileSync("./sample.txt").toString().split("\n");
+const [input, d] = fs.readFileSync("./sample.txt").toString().split("\n");
 
-function longCommonSubsequence(str1, str2) {
-  if (str1 === str2) {
-    return str1.length;
-  }
+const arr = input.split(" ").map((el) => +el);
 
-  dp = [...new Array(str1.length + 1)].map((el) =>
-    new Array(str2.length + 1).fill(0)
-  );
-  for (let i = 1; i < str1.length + 1; i++) {
-    for (let j = 1; j < str2.length + 1; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-      } else {
-        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-      }
+function solution(a, d) {
+  let total_sec = d;
+  let hour = 0;
+  let minute = 0;
+  let second = 0;
+  while (total_sec > 0) {
+    if (total_sec >= 3600) {
+      hour = Math.floor(total_sec / 3600);
+      total_sec = total_sec % 3600;
+    } else if (total_sec >= 60) {
+      minute = Math.floor(total_sec / 60);
+      total_sec = total_sec % 60;
+    } else {
+      second = total_sec;
+      break;
     }
   }
-  return dp[str1.length][str2.length];
+  return [hour, minute, second];
 }
 
-console.log(longCommonSubsequence(a, b));
+function check(h, m, s) {
+  let hour = h;
+  let minute = m;
+  let second = s;
+  while (hour > 23 || minute > 59 || second > 59) {
+    if (hour > 23) {
+      hour = hour - 24;
+    } else if (minute > 59) {
+      minute = minute - 60;
+      hour += 1;
+    } else if (second > 59) {
+      second = second - 60;
+      minute += 1;
+    }
+  }
+  return [hour, minute, second];
+}
+
+const [hour, minute, second] = solution(arr, +d);
+const [h, m, s] = check(arr[0] + hour, arr[1] + minute, arr[2] + second);
+console.log(h, m, s);
