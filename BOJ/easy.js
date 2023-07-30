1e1862706ed06fcdb5786de39ccf78fcd86f6d7d
isabel-noh <nohhyunjeong93@gmail.com>
2023-07-30 17:31:55 +0900

    change

--- BOJ/easy.js
+++ BOJ/easy.js
@@ -1,66 +1,17 @@
-class Heap {
-  constructor() {
-    this.heap = [];
-  }
+const fs = require("fs");
+const str = fs.readFileSync("./sample.txt").toString();
+const [A, C] = str.split("\n");
+let [a, b] = A.split(" ").map((el) => +el);
+let c = +C;
 
-  getParentIndex(i) {
-    return Math.floor(i / 2);
-  }
-
-  getLeftChildIndex(i) {
-    return i * 2;
-  }
-
-  getRightChildIndex(i) {
-    return i * 2 + 1;
-  }
-
-  swap(idx1, idx2) {
-    const temp = this.heap[idx1];
-    this.heap[idx1] = this.heap[idx2];
-    this.heap[idx2] = temp;
-  }
-
-  push(val) {
-    this.heap[this.heap.length] = val;
-    this.heapifyUp();
-  }
-
-  heapifyUp() {
-    let curIdx = this.heap.length - 1;
-    while (this.heap[curIdx] > this.heap[this.getParentIndex(curIdx)]) {
-      this.swap(curIdx, this.getParentIndex(curIdx));
-      curIdx = this.getParentIndex(curIdx);
-    }
-  }
-
-  poll() {
-    let maxVal = this.heap[0];
-    if (maxVal === 12 || maxVal === 10 || maxVal === 8) {
-      console.log("g", this.heap);
-    }
-    this.heap[0] = this.heap[this.heap.length - 1];
-    this.heap.length--;
-    this.heapifyDown();
-    return maxVal;
-  }
-
-  heapifyDown() {
-    let curIdx = 0;
-    while (this.heap[this.getLeftChildIndex(curIdx)] !== undefined) {
-      let biggerChild = this.getLeftChildIndex(curIdx);
-      if (
-        this.heap[this.getLeftChildIndex(curIdx)] <
-        this.heap[this.getRightChildIndex(curIdx)]
-      ) {
-        biggerChild = this.getRightChildIndex(curIdx);
-      }
-      if (this.heap[curIdx] < this.heap[biggerChild]) {
-        this.swap(curIdx, biggerChild);
-        curIdx = biggerChild;
-      } else {
-        return;
-      }
-    }
-  }
+b = b + c;
+if (b > 59) {
+  let nameoji = b % 60;
+  let mok = Math.floor(b / 60);
+  b = nameoji;
+  a += mok;
 }
+if (a > 23) {
+  a -= 24;
+}
+console.log(a, b);
