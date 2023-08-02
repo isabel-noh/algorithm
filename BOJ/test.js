0f5b05012b8d8bda106d51a48f0d2dbd75194aa5
isabel-noh <nohhyunjeong93@gmail.com>
2023-08-03 03:52:45 +0900

    greedy silver 3

--- BOJ/test.js
+++ BOJ/test.js
@@ -1,38 +1,5 @@
-function check(info, query) {
-  let flag = true;
-  for (condition of query) {
-    if (flag === false) {
-      return false;
-    }
-    if (!isNaN(parseInt(condition))) {
-      if (Number(condition) > Number(info[info.length - 1])) {
-        flag = false;
-      }
-    } else {
-      if (!info.includes(condition)) {
-        flag = false;
-      }
-    }
-  }
-  if (flag === false) {
-    return false;
-  }
-  return true;
-}
-function solution(info, query) {
-  var answer = [];
-  const infoArr = info.map((el) => el.split(" "));
-  const queryArr = query.map((el) =>
-    el.split(" ").filter((v) => v !== "and" && v !== "-")
-  );
-  for (const iterator of queryArr) {
-    let candidate = 0;
-    for (const item of infoArr) {
-      if (check(item, iterator)) {
-        candidate++;
-      }
-    }
-    answer.push(candidate);
-  }
-  return answer;
-}
+const arr = new Array(5).fill();
+
+arr.forEach((v, i, array) => (array[i] = () => i));
+console.log(arr);
+arr.forEach((f) => console.log(f()));
