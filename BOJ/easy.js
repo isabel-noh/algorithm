1b3ba64e8e98237904c436ef5e60b4338cafe304
isabel-noh <nohhyunjeong93@gmail.com>
2023-08-02 14:05:14 +0900

    greedy

--- BOJ/easy.js
+++ BOJ/easy.js
@@ -1,17 +1,33 @@
 const fs = require("fs");
-const str = fs.readFileSync("./sample.txt").toString();
-const [A, C] = str.split("\n");
-let [a, b] = A.split(" ").map((el) => +el);
-let c = +C;
+const [n, input] = fs.readFileSync("./sample.txt").toString().split("\n");
+const N = +n;
+const arr = input.split(" ").map((el) => +el);
 
-b = b + c;
-if (b > 59) {
-  let nameoji = b % 60;
-  let mok = Math.floor(b / 60);
-  b = nameoji;
-  a += mok;
+// 맨 처음에는 딸기우유를 한 팩 마신다.
+// 딸기우유를 한 팩 마신 후에는 초코우유를 한 팩 마신다.
+// 초코우유를 한 팩 마신 후에는 바나나우유를 한 팩 마신다.
+// 바나나우유를 한 팩 마신 후에는 딸기우유를 한 팩 마신다.
+let drink = 0;
+let whatDrank = 0;
+for (const el of arr) {
+  if (drink === 0) {
+    if (el === 0) {
+      drink++;
+    } else {
+      continue;
+    }
+  } else {
+    if (whatDrank === 0 && el === 1) {
+      drink++;
+      whatDrank = 1;
+    } else if (whatDrank === 1 && el === 2) {
+      drink++;
+      whatDrank = 2;
+    } else if (whatDrank === 2 && el === 0) {
+      drink++;
+      whatDrank = 0;
+    }
+  }
 }
-if (a > 23) {
-  a -= 24;
-}
-console.log(a, b);
+
+console.log(drink);
