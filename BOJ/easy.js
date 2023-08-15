381b66f765cd3d3bffef755585ca9f47bc9ad284
isabel-noh <nohhyunjeong93@gmail.com>
2023-08-16 00:28:24 +0900

    string

--- BOJ/easy.js
+++ BOJ/easy.js
@@ -1,33 +1,13 @@
 const fs = require("fs");
-const [n, input] = fs.readFileSync("./sample.txt").toString().split("\n");
-const N = +n;
-const arr = input.split(" ").map((el) => +el);
-
-// 맨 처음에는 딸기우유를 한 팩 마신다.
-// 딸기우유를 한 팩 마신 후에는 초코우유를 한 팩 마신다.
-// 초코우유를 한 팩 마신 후에는 바나나우유를 한 팩 마신다.
-// 바나나우유를 한 팩 마신 후에는 딸기우유를 한 팩 마신다.
-let drink = 0;
-let whatDrank = 0;
-for (const el of arr) {
-  if (drink === 0) {
-    if (el === 0) {
-      drink++;
-    } else {
-      continue;
-    }
-  } else {
-    if (whatDrank === 0 && el === 1) {
-      drink++;
-      whatDrank = 1;
-    } else if (whatDrank === 1 && el === 2) {
-      drink++;
-      whatDrank = 2;
-    } else if (whatDrank === 2 && el === 0) {
-      drink++;
-      whatDrank = 0;
-    }
+let n = fs.readFileSync("./sample.txt").toString().trim();
+let num = +n;
+let cnt = 0;
+while (true) {
+  cnt++;
+  temp = Math.floor(num / 10) + (num % 10);
+  num = (num % 10) * 10 + (temp % 10);
+  if (num === Number(n)) {
+    break;
   }
 }
-
-console.log(drink);
+console.log(cnt);
