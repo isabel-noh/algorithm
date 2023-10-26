a71cd3b50abb7566ce4fbc4d426ca3de3738db59
isabel-noh <nohhyunjeong93@gmail.com>
2023-10-27 03:05:16 +0900

    brute force

--- Progrmmers/test.js
+++ Progrmmers/test.js
@@ -1,48 +1,47 @@
-let unionFinds = [];
-const find = (a) => {
-  if (unionFinds[a] < 0) {
-    return a;
+function solution(word) {
+  var answer = 0;
+  // A E I O U
+  // 길이 5 이하의 모든 단어
+  // A, AA, AAA, AAAA, AAAAA, AAAAE, ... , UUUUU
+  const arr = ["A", "E", "I", "O", "U"];
+  for (let i = 0; i < word.length; i++) {
+    if (i === 0) {
+      for (let l = 0; l < 5; l++) {
+        if (word[i] === arr[l]) {
+          answer += 1 + 781 * l;
+        }
+      }
+    } else if (i === 1) {
+      for (let l = 0; l < 5; l++) {
+        if (word[i] === arr[l]) {
+          answer += 1 + 156 * l;
+        }
+      }
+    } else if (i === 2) {
+      for (let l = 0; l < 5; l++) {
+        if (word[i] === arr[l]) {
+          answer += 1 + 31 * l;
+        }
+      }
+    } else if (i === 3) {
+      for (let l = 0; l < 5; l++) {
+        if (word[i] === arr[l]) {
+          answer += 1 + 6 * l;
+        }
+      }
+    } else if (i === 4) {
+      for (let l = 0; l < 5; l++) {
+        if (word[i] === arr[l]) {
+          answer += 1 + 1 * l;
+        }
+      }
+    }
   }
-  unionFinds[a] = find(unionFinds[a]);
-  return unionFinds[a];
-};
-
-const merge = (a, b) => {
-  const fa = find(a);
-  const fb = find(b);
-  if (fa === fb) return;
-
-  unionFinds[fa] += unionFinds[fb];
-  unionFinds[fb] = fa;
-};
-
-function solution(n, wires) {
-  let result = Infinity;
-  const N = wires.length;
-
-  for (let node = 0; node < N; node++) {
-    unionFinds = new Array(n + 1).fill(-1);
-    console.log("unionFinds", unionFinds);
-    const graph = wires.filter((_, i) => i != node); // 줄 끊기
-    console.log(graph);
-
-    for (const [v, w] of graph) merge(v, w);
-    console.log(unionFinds);
-
-    const [a, b] = unionFinds.slice(1).filter((v) => v < 0);
-    console.log("a,b", a, b);
-    result = Math.min(result, Math.abs(a - b));
-  }
-  return result;
+  console.log(answer);
+  return answer;
 }
 
-solution(9, [
-  [1, 3],
-  [2, 3],
-  [3, 4],
-  [4, 5],
-  [4, 6],
-  [4, 7],
-  [7, 8],
-  [7, 9],
-]);
+solution("AAAAE");
+solution("AAAE");
+solution("I");
+solution("EIO");
