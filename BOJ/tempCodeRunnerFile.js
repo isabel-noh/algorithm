5455a19dd6a82d5a65a200105122694fe93d5f2a
isabel-noh <nohhyunjeong93@gmail.com>
2024-07-05 16:44:34 +0900

    dp

--- BOJ/tempCodeRunnerFile.js
+++ BOJ/tempCodeRunnerFile.js
@@ -1,3 +1,16 @@
+(arr);
 
-    arr += input[j][i];
+const check = new Array(N + 1).fill(0);
+for (let i = 0; i < N - 1; i++) {
+  if (musics[i] > musics[i + 1]) {
+    check[i + 1] = i === 0 ? 1 : check[i] + 1;
+  } else {
+    check[i + 1] = i === 0 ? 0 : check[i];
   }
+}
+console.log(check);
+
+for (let i = 0; i < Q; i++) {
+  const [x, y] = arr[i];
+  console.log(check[y - 1] - check[x - 1]);
+}
