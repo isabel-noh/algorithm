75537939eb49609863a3ae6e47693484ec9fa3e7
isabel-noh <nohhyunjeong93@gmail.com>
2024-07-09 21:31:14 +0900

    dp

--- BOJ/tempCodeRunnerFile.js
+++ BOJ/tempCodeRunnerFile.js
@@ -1,16 +1,31 @@
-(arr);
 
-const check = new Array(N + 1).fill(0);
-for (let i = 0; i < N - 1; i++) {
-  if (musics[i] > musics[i + 1]) {
-    check[i + 1] = i === 0 ? 1 : check[i] + 1;
-  } else {
-    check[i + 1] = i === 0 ? 0 : check[i];
+const arr = input.split(" ").map(Number);
+const dp = [...new Array(+n)].fill(0);
+const countDp = [...new Array(+n)].fill(1);
+dp[0] = arr[0];
+for (let i = 1; i < +n; i++) {
+  let maxIdx = 0;
+  let maxValue = 0;
+  for (let j = 0; j < i; j++) {
+    if (maxValue <= dp[j]) {
+      maxValue = dp[j];
+      maxIdx = j;
+    }
   }
+  console.log(i, maxIdx, maxValue);
+  if (maxValue >= arr[i]) {
+    dp[i] = maxValue + arr[i];
+    countDp[i] = countDp[maxIdx] + 1;
+  } else {
+    dp[i] = arr[i];
+  }
+  //   if (dp[i - 1] >= arr[i]) {
+  //     dp[i] = dp[i - 1] + arr[i];
+  //     countDp[i] += 1;
+  //   } else {
+  //     dp[i] = arr[i];
+  //   }
 }
-console.log(check);
-
-for (let i = 0; i < Q; i++) {
-  const [x, y] = arr[i];
-  console.log(check[y - 1] - check[x - 1]);
-}
+console.log(dp);
+console.log(countDp);
+console.log(Math.max(...countDp));
